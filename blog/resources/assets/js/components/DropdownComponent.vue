81f00b1176f0a385de61ff209fd5b54f36253346
yamauchi.ryoji <yamauchi.ryoji@m-field.co.jp>
2018-04-06 12:21:57 +0900

    画像アップロード機能を追加

--- blog/resources/assets/js/components/DropdownComponent.vue
+++ blog/resources/assets/js/components/DropdownComponent.vue
@@ -1,10 +1,13 @@
 <template>
     <!--自身をクリックしたときは、クローズしないようにするため、stopでclickイベントを止めてる-->
-    <div class="menu" @click.stop>
-            <div class="image" v-for="(url, index) in images" :key="index">
-                <img :src="url"  width="65px" height="65px"/>
-            </div>
-    </div>    
+    <div class="menu" @click.stop @dragleave.prevent @dragover.prevent @drop.prevent="onDrop">
+        <div class="images" >
+                <div class="image" v-for="(url, index) in images" :key="index">
+                    <img :src="url"  width="65px" height="65px"/>
+                </div>
+        </div>    
+       <div class="msg-error">{{ errorMsg }}</div>
+    </div>
 </template>
 
 <script>
@@ -13,9 +16,36 @@ export default {
     props:{
         images:Array
     },
+    data:function(){
+        return{
+            msg:'',
+            errorMsg:'',
+        }
+    },
     methods:{
+        //コンポーネントをクローズする
         close:function(){
             this.$emit('close');
+        },
+        //ファイルドロップ
+        onDrop:function(event){
+            let fileList = event.target.files ? 
+               event.target.files:
+               event.dataTransfer.files;
+
+            let file = fileList[0];
+        
+            let formData = new FormData;
+            formData.append('image', file);
+
+            axios.post('', formData)
+            .then((res)=>{
+                console.log(res);
+            })
+            .catch((error)=>{
+                console.log(error);
+                this.errorMsg = 'アップロードに失敗しました。'
+            });
         }
     },
     created:function(){
@@ -32,11 +62,20 @@ export default {
 
 <style lang="scss" scoped>
     .menu{
-        display: flex;
-        flex-wrap: wrap;
         width:200px;
         height:200px;
         background-color: white;
+    }
+
+    .images{
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .msg{
+        &-error{
+            color:red;
+        }
     }
 </style>
 
